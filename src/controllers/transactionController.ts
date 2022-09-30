864b9a5ee575d4cd410d254fa642a45d3ce060b1
Alessandro <alessandro.mello@codezone.com.br>
2022-09-30 16:24:36 -0300

    Refac[AddTrasaction] id_seller by product

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,3 +1,4 @@
+import { Product } from './../models/product';
 import { Request , Response } from "express";
 import { Transaction } from '../models/transaction';
 import { soldProduct } from "./productController";
@@ -13,13 +14,22 @@ export const listTransaction = async (req: Request , res: Response) => {
 }
 
 export const addTransaction = async (req: Request , res: Response) => {
-    let {id_seller , id_product} = req.body;
+    let {id_product} = req.body;
     let id_buyer = res.locals.user.id;
 
-    let newTransaction = await Transaction.create({id_seller,id_buyer,id_product})
-    .then(async()=>{
-        await soldProduct(id_product);
-    })
+    let product = await Product.findByPk(id_product);
 
+    if(product && id_buyer != product.id_user){
+        let newTransaction = await Transaction.create({id_seller: product.id_user, id_buyer, id_product})
+        .then(async()=>{
+            await soldProduct(id_product);
+        })
+    
+        res.status(201)
     res.json(newTransaction)
+    } else {
+        res.status(400)
+        res.json({status: false})
+    }
+   
 }
